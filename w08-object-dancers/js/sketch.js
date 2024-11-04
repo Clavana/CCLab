82c46621da0d3e8ca2a209ee15edcdf5f56b9b1c
Clavana <marlena.soltysinska@gmail.com>
2024-11-05 02:05:32 +0800

    happy

--- w08-object-dancers/js/sketch.js
+++ w08-object-dancers/js/sketch.js
@@ -4,79 +4,89 @@ function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5-canvas-container");
 
-  dancer = new MoonDancer(width / 2, height / 2);
+  dancer = new MarlenaDancer(width / 2, height / 2);
 }
 
 function draw() {
   background(0);
-  //drawFloor(); // for reference only
-
   dancer.update();
   dancer.display();
 }
 
-
-
-
-
-class MoonDancer {
+class MarlenaDancer {
   constructor(startX, startY) {
+    this.originX = startX;
+    this.originY = startY;
     this.x = startX;
     this.y = startY;
-
     this.r = 255;
     this.g = 255;
     this.b = 255;
+    this.counter = 0;
+    this.directionX = random(-10,10);
+    this.directionY = random(-10,10);
   }
+
   update() {
-    this.r = map(sin(frameCount * 0.07), -1, 1, 0, 255);
-    this.g = map(mouseX, 0, width, 0, 255);
-    this.b = map(sin(frameCount * 0.06), -1, 1, 0, 255);
+    this.r = random(0, 255);
+    this.g = random(0, 255);
+    this.b = random(0, 255);
+    
+    this.x += this.directionX;
+    this.y += this.directionY;
+    
+    if (this.x + 30 > this.originX + 100) {
+      this.directionX = random(-10,-1);
+      this.counter++;
+    }
+    if (this.x - 30 < this.originX - 100) {
+      this.directionX = random(1,10);
+      this.counter++;
+    }
+    if (this.y + 30 > this.originY + 100) {
+      this.directionY = random(-10,-1);
+      this.counter++;
+    }
+    if (this.y - 30 < this.originY - 100) {
+      this.directionY = random(1,10);
+      this.counter++;
+    }
   }
+
   display() {
     push();
     translate(this.x, this.y);
-
-    this.drawBody();
-
-    this.drawLimb(40, -20, radians(random(-10, 10)));
-    this.drawLimb(40, 0, radians(random(-10, 10)));
-    this.drawLimb(40, 20, radians(random(-10, 10)));
-
-    this.drawLimb(-40, -20, radians(random(170, 190)));
-    this.drawLimb(-40, 0, radians(random(170, 190)));
-    this.drawLimb(-40, 20, radians(random(170, 190)));
-
-    this.drawReferenceShapes();
+    this.drawFace(0, 0);
     pop();
+    push()
+    translate(this.originX, this.originY)
+    this.drawReferenceShapes();
+    pop()
   }
-  drawLimb(x, y, angle) {
+
+  drawFace(x, y) {
     push();
     translate(x, y);
-    rotate(angle)
-    strokeWeight(10);
-    stroke(255, 0, 255);
-    line(0, 0, 100, 0);
+    fill(this.r, this.g, this.b);
+    circle(0, 0, 50);
+    fill(0);
+    ellipse(-10, -10, 5, 10);
+    ellipse(10, -10, 5, 10);
+    noFill();
+    stroke(0);
+    strokeWeight(4);
+    let mouthStart = this.counter < 10 ? 0 : PI;
+    let mouthEnd = this.counter < 10 ? PI : TWO_PI;
+    if (this.counter == 20) {
+      this.counter = 0
+    }
+    arc(0, 10, 25, 15, mouthStart, mouthEnd);
     pop();
   }
-  drawBody() {
-    let sinValue = sin(frameCount * 0.15) * 30;
-    noStroke();
-    fill(this.r, this.g, this.b);
-    circle(0 - sinValue, 50, 100);
-    fill(this.b, this.r, this.g);
-    circle(0, 0, 100);
-    fill(this.g, this.b, this.r);
-    circle(0 + sinValue, -50, 100);
-  }
+
   drawReferenceShapes() {
     noFill();
-    stroke(255, 0, 0);
-    line(-5, 0, 5, 0);
-    line(0, -5, 0, 5);
     stroke(255);
     rect(-100, -100, 200, 200);
-    fill(255);
-    stroke(0);
   }
 }
